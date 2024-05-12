fb39bd9294b12142e1a9cac3e3b516b742ce5b9d
mrx7355608 <mrx7355608@gmail.com>
2024-05-13 00:00:58 +0500

    added exception handling

--- scrape.py
+++ scrape.py
@@ -1,9 +1,5 @@
-import requests
-import time
-import json
+import json, requests
 from bs4 import BeautifulSoup as bs4
-from requests.exceptions import ConnectTimeout, ConnectionError
-from urllib3.exceptions import ProtocolError
 
 from data_extractor import DataExtractor
 from fetch_urls import FetchUrls
@@ -36,41 +32,65 @@ def main():
     counter = 0
     while counter < len(urls):
         url = urls[counter]
-        response = fetcher.get(url)
-        soup = bs4(response.content, "html.parser")
-        extractor.extract_lengths_with_links(soup)
 
-        # extract lengths and their links
-        for idx, length_link in enumerate(extractor.lengths_links):
-            if idx == 2:
-                break
-            content = {
-                "product_name": "",
-                "product_code": "",
-                "product_description": "",
-                "product_features": [],
-                "image_name": "",
-                "variants": [],
-                "specifications": [],
-            }
-            resp2 = fetcher.get(length_link)
-            soup2 = bs4(resp2.content, "html.parser")
-            extractor.extract_colors_with_links(soup2)
-            extractor.extract_product_details(soup2, content)
+        try:
+            response = fetcher.get(url)
+            soup = bs4(response.content, "html.parser")
 
-            # extract all colors and their links for every length
-            for index, color_link in enumerate(extractor.colors_links):
-                resp3 = fetcher.get(color_link)
-                soup3 = bs4(resp3.content, "html.parser")
-                # extract data
-                extractor.extract_color_data(soup3, content, extractor.colors[index])
-            extractor.empty_colors_list()
+            # extract lengths and their links
+            extractor.extract_lengths_with_links(soup)
+
+            # extract data for individual length
+            for idx, length_link in enumerate(extractor.lengths_links):
+                if idx == 2:
+                    break
+                content = {
+                    "product_name": "",
+                    "product_code": "",
+                    "product_description": "",
+                    "category_1": "",
+                    "category_2": "",
+                    "product_features": [],
+                    "image_name": "",
+                    "variants": [],
+                    "specifications": [],
+                }
+                resp2 = fetcher.get(length_link)
+                soup2 = bs4(resp2.content, "html.parser")
+                extractor.extract_colors_with_links(soup2)
+                extractor.extract_product_details(soup2, content)
+
+                # extract all colors and their links for every length
+                for index, color_link in enumerate(extractor.colors_links):
+                    resp3 = fetcher.get(color_link)
+                    soup3 = bs4(resp3.content, "html.parser")
+                    # extract data
+                    extractor.extract_color_data(
+                        soup3, content, extractor.colors[index], extractor.lengths[idx]
+                    )
+                extractor.empty_colors_list()
+                extractor.empty_colors_links_list()
+                data.append(content)
+
+            extractor.empty_lengths_list()
             extractor.empty_colors_links_list()
-            data.append(content)
+            counter += 1
 
-        extractor.empty_lengths_list()
-        extractor.empty_colors_links_list()
-        counter += 1
+        except requests.exceptions.RequestException as e:
+            # print error message
+            print(f"ERROR: {str(e)}")
+            print("Trying next url")
+            continue
+
+        except requests.exceptions.ConnectionError:
+            print("ERROR: Lost internet connection")
+            # save already scraped data
+            if len(data) < 1:
+                print("No data was scraped")
+            else:
+                print("Saving already scraped data...")
+                with open("scrape_fail_data.json", "a+") as file:
+                    json.dump(data, file, indent=4)
 
         # save data in a data.json file
         with open("content.json", "a+") as file:
@@ -80,51 +100,3 @@ def main():
 
 
 main()
-
-
-# Main scraping process
-# response = requests.get("")
-# if response.ok:
-#     soup = bs4(response.content, "html.parser")
-#     extract_lengths(soup)
-#
-#     counter = 0
-#     while counter != len(lengths_links):
-#         content = {'length': "", 'colors': []}
-#         content["length"] = lengths[counter]
-#
-#         resp = requests.get(lengths_links[counter], timeout=10)
-#         if resp.ok:
-#             soup = bs4(resp.content, "html.parser")
-#             extract_colors(soup)
-#
-#             counter2 = 0
-#             while counter2 != len(colors_links):
-#                 retries = 3
-#                 delay = 1
-#                 while retries > 0:
-#                     try:
-#                         print(f"Extracting data for:\nLength: {lengths[counter]}\nColor: {colors[counter2]}")
-#                         resp2 = requests.get(colors_links[counter2], timeout=10)
-#                         if resp2.ok:
-#                             soup2 = bs4(resp2.content, "html.parser")
-#                             extract_color_data(soup2, content, counter2)
-#                             break
-#                         else:
-#                             print(f"Failed to retrieve data for URL: {colors_links[counter2]}")
-#                     except (ConnectTimeout, ConnectionError, ProtocolError) as e:
-#                         print(f"Connection error occurred: {e}")
-#                     retries -= 1
-#                     time.sleep(delay)
-#                     delay *= 2
-#                 counter2 += 1
-#
-#             data.append(content)
-#             colors_links = []
-#             counter += 1
-#         else:
-#             print(f"Failed to retrieve data for URL: {lengths_links[counter]}")
-#
-# else:
-#     print("Failed to retrieve initial page data.")
-#
