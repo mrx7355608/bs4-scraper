27582856222a29c2a7826daab7d147e36cb30f9a
mrx7355608 <mrx7355608@gmail.com>
2024-05-21 16:14:44 +0500

    added a scrape_specs method in base_scraper

--- base_scraper.py
+++ base_scraper.py
@@ -5,7 +5,7 @@ class BaseScraper:
     def scrape_name(self, soup) -> str:
         product_name = soup.find("h1", {"class": "name"}).text
         return product_name
-        
+
     def scrape_code(self, soup) -> str:
         product_code = soup.find(
             "span", {"class": "lr-product-info--item sku js-code-switch"}
@@ -22,3 +22,27 @@ class BaseScraper:
             soup.find("span", {"class": "listed-price"}).text.strip() or "not available"
         )
         return price
+
+    def scrape_specifications(self, soup):
+        # extract headlines
+        all_headlines = soup.find_all("div", {"class": "headline"})
+        headlines_list = []
+        specifications = []
+        for headline in all_headlines:
+            headline = headline.text.strip()
+            spec_headline = {}
+            spec_headline[headline] = []
+            headlines_list.append(headline)
+            specifications.append(spec_headline)
+
+        # extract specs tables
+        all_tables = soup.find_all("table", {"class": "table"})
+        for index, table in enumerate(all_tables):
+            keys = table.find_all("td", {"class": "attrib"})
+            for key in keys:
+                value = key.find_next("td").text.strip()
+                spec = {"title": key.text.strip(), "value": value}
+
+                headline = headlines_list[index]
+                specifications[index][headline].append(spec)
+        return specifications
