2eab6a59fcd03120c4d6bfc9d64bccff581fda5e
mrx7355608 <mrx7355608@gmail.com>
2024-05-21 16:15:10 +0500

    hardcoded user-agent in requests headers

--- fetch_urls.py
+++ fetch_urls.py
@@ -3,15 +3,14 @@ import time
 
 
 class FetchUrls:
-    def __init__(self,  userAgent: str):
+    def __init__(self):
         self.session = requests.Session()
-        self.user_agent = userAgent
 
     def get(self, url):
         time.sleep(1)
 
         headers = {
-            "User-Agent": self.user_agent
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
         }
         response = self.session.get(url, headers=headers)
         return response.content
