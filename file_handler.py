fdfcf8eb012363964be9f0da2c49607ee9d5e01f
mrx7355608 <mrx7355608@gmail.com>
2024-05-21 16:15:29 +0500

    fixed a mistake in read_urls methods of FileHandler

--- file_handler.py
+++ file_handler.py
@@ -2,6 +2,7 @@ from typing import List
 import csv
 import json
 
+
 class FileHandler:
     def __init__(self) -> None:
         pass
@@ -11,17 +12,17 @@ class FileHandler:
         with open("urls.csv") as file:
             reader = csv.reader(file)
             for index, lines in enumerate(reader):
-                if index != 0 or lines[0] != "":
-                    urls.append(lines[0])            
+                if index != 0 and lines[0] != "":
+                    urls.append(lines[0])
         return urls
-    
+
     def save_logs(self, logsList: List[str]):
         with open("logs.txt", "a+") as file:
             for logs in logsList:
                 file.write(logs)
         print("[INFO] logs.txt has been created")
         return
-    
+
     def save_scraped_data(self, data: List[dict]) -> None:
         with open("content.json", "a+") as file:
             json.dumps(data, file, indent=4)
