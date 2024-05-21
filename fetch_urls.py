7dec4b03c37330dad294318b8c997caa706df3a6
mrx7355608 <mrx7355608@gmail.com>
2024-05-21 17:37:24 +0500

    returning soup

--- fetch_urls.py
+++ fetch_urls.py
@@ -1,16 +1,18 @@
 import requests
 import time
+from bs4 import BeautifulSoup as bs4
 
 
 class FetchUrls:
     def __init__(self):
         self.session = requests.Session()
 
-    def get(self, url):
+    def get_soup(self, url):
         time.sleep(1)
 
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
         }
         response = self.session.get(url, headers=headers)
-        return response.content
+        soup = bs4(response.content, "lxml")
+        return soup
